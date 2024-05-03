ec082a90760bfde490f9b974e84d436a9b638675
Santhuns2001 <n.s.santhosh116@gmail.com>
2024-05-03 09:59:41 +0530

    added ddl triggers and made some modifications in all triggers file

---
+++ DDL_Triggers.sql
@@ -0,0 +1,141 @@
+
+
+-- DDL Operations
+
+create table test
+(id int)
+
+-- trigger for creating table
+create trigger trgPreventCreateTable
+on database 
+for create_table
+as
+begin
+print 'you cannot create the in this database'
+rollback transaction
+end
+
+drop trigger trgPreventCreateTable  on database
+
+create table test2 (id int)
+  
+  -- trigger for altertion
+
+create trigger trgPreventAlterTable 
+on database 
+for alter_table
+as
+begin
+print 'you cannot alter any of the table in this database'
+rollback transaction
+end
+
+drop trigger trgPreventAlterTable on database
+
+
+alter table test 
+add ename varchar(100)
+
+-- drop trigger 
+
+create trigger trgPreventDropTable
+on database
+for drop_table
+as
+begin
+print 'you cannot delete the table in this database'
+end
+
+
+drop trigger trgPreventDropTable on database
+
+
+-- Event group
+
+create trigger trgDDLTable
+on database
+for create_table, alter_table,drop_table
+as 
+begin
+print 'you cannot  create or alter or delete the table in this database'
+end
+
+drop trigger trgDDLTable on database
+   --or
+create trigger trgDemo
+on database
+for ddl_table_events
+as 
+begin
+print 'you cannot  create or alter or delete the table in this database'
+end
+
+/*
+scope 1. database
+	  2. all server
+
+on database -> can be accessed only within the database 
+on all server -> can be accessed on all specified databases
+*/
+
+-- trigger using all server
+
+create trigger trgScopeDemo
+on all server
+for ddl_table_events
+as 
+begin
+print 'you cannot  create or alter or delete the table in this database'
+end
+
+drop trigger trgScopeDemo on all server
+
+
+
+
+create table DDLAudit
+(
+DatabaseName  nvarchar(250),
+TableName nvarchar(250),
+EventType nvarchar(250),
+LoginName nvarchar(250),
+SQLCommand nvarchar(250),
+AuditDateTime datetime
+
+)
+
+
+
+create database MyDatabase
+use MyDatabase
+
+
+create trigger trgDDLAuditTableChanges
+on all server
+for ddl_table_events
+as
+begin
+declare @EventData  xml;
+select @EventData=EVENTDATA();
+insert into MyDatabase.dbo.DDLAudit (DatabaseName,TableName,EventType,LoginName,SQLCommand,AuditDateTime)
+values(
+@EventData.value('(/EVENT_INSTANCE/DatabaseName)[1]','varchar(250)'),
+@EventData.value('(/EVENT_INSTANCE/ObjectName)[1]','varchar(250)'),
+@EventData.value('(/EVENT_INSTANCE/EventType)[1]','varchar(250)'),
+@EventData.value('(/EVENT_INSTANCE/LoginName)[1]','varchar(250)'),
+@EventData.value('(/EVENT_INSTANCE/TSQLCommand)[1]','varchar(2500)'),
+GETDATE()
+);
+
+end
+
+
+	use MyDatabase
+	create table testing(id int)
+
+	drop table testing
+
+
+	select * from DDLAudit
+
+
--- Triggers.sql
+++ Triggers.sql
@@ -23,10 +23,13 @@ DDL triggers also fire in response to some system stored procedures that perform
 	 CREATE TRIGGER [schema_name.]trigger_name
      ON table_name
      AFTER  {[INSERT],[UPDATE],[DELETE]}
-     [NOT FOR REPLICATION]
+     [NOT FOR REPLICATION]  -> The NOT FOR REPLICATION option instructs SQL Server not to fire the trigger when data modification is made as part of a replication process.
      AS
-     {sql_statements}
+	 begin
+	 SET NONCOUNT ON; -> inside the body of the trigger, you set the SET NOCOUNT to ON to suppress the number of rows affected messages from being returned whenever the trigger is fired.
 
+     {sql_statements}
+	 end
 
 	*/
 
@@ -41,7 +44,8 @@ DDL triggers also fire in response to some system stored procedures that perform
     list_price DEC(10,2) NOT NULL,
     updated_at DATETIME NOT NULL,
     operation CHAR(3) NOT NULL,
-    CHECK(operation = 'INS' or operation='DEL')
+    --CHECK operation in( 'INS','UPD','DEL')
+	check (operation ='INS' OR operation='UPD' OR operation='DEL')
 );
 
 
@@ -58,10 +62,10 @@ product_id int primary key identity(1,1),
 
 CREATE TRIGGER trg_product_audit
 ON products
-AFTER INSERT, DELETE
+AFTER INSERT, DELETE ,UPDATE
 AS
 BEGIN
-    SET NOCOUNT ON;
+    SET NOCOUNT ON;   --
     INSERT INTO product_audits(
         product_id, 
         product_name,
@@ -83,7 +87,10 @@ BEGIN
         'INS'
     FROM
         inserted i
+
     UNION ALL
+	
+	-- DELETE OPERATION
     SELECT
         d.product_id,
         product_name,
@@ -95,8 +102,38 @@ BEGIN
         'DEL'
     FROM
         deleted d;
-		
-END
+
+		UNION ALL
+
+		 SELECT
+        d.product_id,
+        d.product_name,
+        d.brand_id,
+        d.category_id,
+        d.model_year,
+        d.list_price,
+        GETDATE(),
+        'DEL'
+    FROM
+        deleted d
+    WHERE NOT EXISTS (SELECT 1 FROM inserted i WHERE i.product_id = d.product_id) -- Only deletes
+    
+    UNION ALL
+    
+    -- UPDATE operation
+    SELECT
+        i.product_id,
+        i.product_name,
+        i.brand_id,
+        i.category_id,
+        i.model_year,
+        i.list_price,
+        GETDATE(),
+        'UPD'
+    FROM
+        inserted i
+    INNER JOIN deleted d ON i.product_id = d.product_id -- Updated records
+END; 
 
 
 INSERT INTO products(
@@ -127,3 +164,59 @@ WHERE
     product_id = 3;
 	
 	
+
+
+	--creating of INSTEAD OF Trigger
+
+
+	--DISABLING OF Triggers
+	/* syntax
+
+	DISABLE TRIGGER [schema_name.][trigger_name] 
+ON [object_name | DATABASE | ALL SERVER]
+
+In this syntax:
+
+	->First, specify the name of the schema to which the trigger belongs and the name of the 
+		trigger that you want to disable after the DISABLE TRIGGER keywords.
+
+	->Second, specify the table name or view that the trigger was bound to if the trigger is
+		a DML trigger. Use DATABASE if the trigger is DDL database-scoped trigger, 
+		or SERVER if the trigger is DDL server-scoped trigger.
+*/
+
+
+-- disableing of dml triggers
+disable trigger TriggerSchema.trgAfterDelete
+on TriggerSchema.Employee
+
+disable trigger TriggerSchema.trgAfterInsert
+on TriggerSchema.Employee
+
+
+disable trigger TriggerSchema.trgAfterUpdate
+on TriggerSchema.Employee
+
+-- disabling of ddl triggers
+
+disable trigger trgPreventAlterTable
+on database
+
+disable trigger trgPreventCreateTable
+on database
+
+disable trigger trgPreventDropTable
+on database
+
+
+	--ENABLING OF TRIGGERS
+
+
+
+	--Viewing the defination of triggers
+
+
+	--Listing of all triggers in sqlserver
+
+
+	--Removing of triggers
